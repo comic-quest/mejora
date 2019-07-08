5fcf096765955f3ace4663a47cabadb53a8fc58e
comic-quest <nostalenospower@gmail.com>
2019-07-08 19:54:57 +0200

    mejora añadida

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-
+ctx.fillStyle="black";
 ctx.fillRect(0,0,400,200);
+
+var mejora = new Image();
+
+mejora.onload = function(){
+    ctx.drawImage(mejora,canvas.width/2-mejora.width/2,canvas.height/2-mejora.height/2);
+}
+
+mejora.src = "mejora.jpg";
